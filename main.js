61c66b6aeff2151a37f4c4acfdae1325f7fc35e4
MatiasCataldo <maticataldo21@gmail.com>
2023-11-08 12:19:43 -0300

    segundo desafio

--- main.js
+++ main.js
@@ -1,120 +1,133 @@
-class ProductManager{
+const fs = require("fs");
 
+class ProductManager{
     constructor(){
-        this.products = []        
+        this.path = "./productos.txt";
+        this.loadProductsFromFile();
     }
 
-    addProduct(product){
-        if(this.products.length == 0){
-            product.code = 1;
-        } else{
-            product.code = this.products[this.products.length - 1].code + 1;
+    createFileIfNotExists(){
+        if(!fs.existsSync(this.path)){
+            fs.writeFileSync(this.path, "[]", "utf8");
         }
-        if(Object.values(product).includes("")){
+    }
+
+    loadProductsFromFile(){
+        this.createFileIfNotExists();
+        const data = fs.readFileSync(this.path, "utf8");
+        if(data){
+            this.products = JSON.parse(data);
+            this.nextProductId = this.products.length + 1;
+        } else{
+            this.products = [];
+            this.nextProductId = 1;
+        }
+    }
+
+    addProduct(product) {
+        if(Object.values(product).some(value => value === "")){
             console.error("Error al cargar producto! Campos incompletos");
         } else{
+            product.code = this.nextProductId++;
             this.products.push(product);
+            fs.writeFileSync(this.path, JSON.stringify(this.products), "utf8");
         }
     }
 
     getProducts(){
-        return this.products;
+        const data = fs.readFileSync(this.path, "utf8");
+        if(data){
+            const products = JSON.parse(data);
+            return products;
+        } else{
+            return [];
+        }
     }
 
-    getProductById(idProduct){    
-        const existsProduct = this.products.find((product) => product.code === idProduct);
-        if(existsProduct){
-            return existsProduct;
+    getProductById(idProduct){
+        const data = fs.readFileSync(this.path, "utf8");
+        if(data){
+            const products = JSON.parse(data);
+            const existsProduct = products.find((product) => product.code === idProduct);
+            if(existsProduct){
+                return existsProduct;
+            } else{
+                console.log("No Encontrado.");
+            }
         } else{
-            console.log("Not Found.");
+            console.log("No Encontrado.");
+        }
+    }
+
+    updateProduct(idProduct, updatedProduct){
+        const productToUpdate = this.getProductById(idProduct);
+        if(productToUpdate){
+            Object.assign(productToUpdate, updatedProduct);
+            const data = fs.readFileSync(this.path, "utf8");
+            const products = JSON.parse(data);
+            products.forEach((product, index) => {
+                if(product.code === idProduct){
+                    products[index] = productToUpdate;
+                }
+            });
+            fs.writeFileSync(this.path, JSON.stringify(products), "utf8");
+            console.log(`Producto ${idProduct} actualizado.`);
+        } else{
+            console.log("Producto no encontrado. No se pudo actualizar.");
+        }
+    }
+
+    deleteProduct(idProduct){
+        const productToDelete = this.getProductById(idProduct);
+        if(productToDelete){
+            const index = this.products.indexOf(productToDelete);
+            if(index !== -1){
+                this.products.splice(index, 1);
+                console.log(`Producto ${idProduct} eliminado.`);
+                fs.writeFileSync(this.path, JSON.stringify(this.products), "utf8");
+            }
+        } else{
+            console.log("Producto no encontrado. No se pudo eliminar.");
         }
     }
 }
 
 class Product{
-    constructor(
-        title, 
+    constructor(title, 
         description, 
         price, 
         thumbnail, 
-        code, 
-        stock
-        ){
-        this.title = title || "";
-        this.description = description || "";
-        this.price = price || "";
-        this.thumbnail = thumbnail || "";
-        this.code = code || "";
-        this.stock = stock || "";
+        stock) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.stock = stock;
+        this.code = null;
     }
-} 
+}
 
-//Pruebas
+// Pruebas
 const manejadorProductos = new ProductManager();
 
-//PRODUCTO 1:
-console.log(
-    "agregando Producto1: TITLE:'Helado Artesanal de 1/4Kg', DESCRIPTION:'Helado artesanal de 1/4 de kilo con 3 sabores a elegir.', PRICE'1600', URL:'...', ID:'1', STOCK:'50'"
-);
+// PRODUCTO 1:
+console.log("agregando Producto1: TITLE:'Helado Artesanal de 1/4Kg', DESCRIPTION:'Helado artesanal de 1/4 de kilo con 3 sabores a elegir.', PRICE'1600', URL:'...', ID:'1', STOCK:'50'");
 
-manejadorProductos.addProduct(
-    new Product("Helado Artesanal de 1/4Kg", "Helado artesanal de 1/4 de kilo con 3 sabores a elegir.", 1600, "url", 0, 50)
-);
+manejadorProductos.addProduct(new Product("Helado Artesanal de 1/4Kg", "Helado artesanal de 1/4 de kilo con 3 sabores a elegir.", 1600, "url", 0, 50));
 
-//PRODUCTO 2:
-console.log(
-    "agregando Producto2: TITLE:'Helado Artesanal de 1/2Kg', DESCRIPTION:'Helado artesanal de 1/2 de kilo con 3 sabores a elegir.', PRICE'2900', URL:'...', ID:'1', STOCK:'50'"
-);
+// PRODUCTO 2:
+console.log("agregando Producto2: TITLE:'Helado Artesanal de 1/2Kg', DESCRIPTION:'Helado artesanal de 1/2 de kilo con 3 sabores a elegir.', PRICE'2900', URL:'...', ID:'1', STOCK:'50'");
 
-manejadorProductos.addProduct(
-    new Product("Helado Artesanal de 1/2Kg", "Helado artesanal de 1/2 de kilo con 3 sabores a elegir.", 2900, "url", 0, 50)
-);
+manejadorProductos.addProduct(new Product("Helado Artesanal de 1/2Kg", "Helado artesanal de 1/2 de kilo con 3 sabores a elegir.", 2900, "url", 0, 50));
 
-//PRODUCTO 3:
-console.log(
-    "agregando Producto3: TITLE:'Helado Artesanal de 1Kg', DESCRIPTION:'Helado artesanal de 1 kilo con 4 sabores a elegir.', PRICE'4800', URL:'...', ID:'1', STOCK:'50'"
-);
+// PRODUCTO 3:
+console.log("agregando Producto3: TITLE:'Helado Artesanal de 1Kg', DESCRIPTION:'Helado artesanal de 1 kilo con 4 sabores a elegir.', PRICE'4800', URL:'...', ID:'1', STOCK:'50'");
 
-manejadorProductos.addProduct(
-    new Product("Helado Artesanal de 1Kg", "Helado artesanal de 1 kilo con 4 sabores a elegir.", 4800, "url", 0, 50)
-);
+manejadorProductos.addProduct(new Product("Helado Artesanal de 1Kg", "Helado artesanal de 1 kilo con 4 sabores a elegir.", 4800, "url", 0, 50));
 
-//PRODUCTO 4 INCOMPLETO:
-console.log(
-    "agregando Producto4: TITLE:'', DESCRIPTION:'Helado artesanal de 1 kilo con 4 sabores a elegir.', PRICE'4800', URL:'...', ID:'1', STOCK:'50'"
-);
+// PRODUCTO 4 INCOMPLETO:
+console.log("agregando Producto4: TITLE:'', DESCRIPTION:'Helado artesanal de 1 kilo con 4 sabores a elegir.', PRICE'4800', URL:'...', ID:'1', STOCK:'50'");
 
-manejadorProductos.addProduct(
-    new Product("", "Helado artesanal de 1 kilo con 4 sabores a elegir.", 4800, "url", 0, 50)
-);
+manejadorProductos.addProduct(new Product("", "Helado artesanal de 1 kilo con 4 sabores a elegir.", 4800, "url", 0, 50));
 
 console.log(manejadorProductos.getProducts());
-
-// Obtén todos los botones "Comprar" y los popups de productos
-const buyButtons = document.querySelectorAll('.buy-button');
-const productPopups = document.querySelectorAll('.product-popup');
-
-buyButtons.forEach((buyButton, index) => {
-    buyButton.addEventListener('click', () => {
-        // Muestra el popup correspondiente al botón "Comprar" clicado
-        productPopups[index].style.display = 'block';
-    });
-});
-
-// Cuando se hace clic en "Agregar al carrito"
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-addToCartButtons.forEach((addToCartButton) => {
-    addToCartButton.addEventListener('click', () => {
-        // Recopila las opciones del producto, por ejemplo, los sabores seleccionados
-        const selectedFlavors = Array.from(document.querySelectorAll('input[name="flavor"]:checked'))
-            .map((flavor) => flavor.value);
-
-        // Realiza la lógica para agregar el producto al carrito, por ejemplo, guardar en una lista
-        // y actualizar el contador del carrito
-        // ...
-
-        // Cierra el popup
-        addToCartButton.closest('.product-popup').style.display = 'none';
-    });
-});
