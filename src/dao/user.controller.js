ab81de8fad239645d7ec8a8a85c626418cff76d3
MatiasCataldo <maticataldo21@gmail.com>
2024-01-07 22:47:53 -0300

    crear primer user

--- src/dao/user.controller.js
+++ src/dao/user.controller.js
@@ -5,30 +5,16 @@ class UserController {
         try {
             const user = await userDao.createUser( username);
             await userDao.createNewCart(user._id);
-            res.status(201).json({
-                status: 'success',
-                user,
-                message: 'Usuario creado!',
-            });
         } catch (error) {
             console.error('Error al crear el primer usuario2:', error);
-            res.status(500).json({ error, message: 'Error al crear el primer usuario2.' });
         }
     }
 
-    async getUserId(req, res) {
+    async getUserId(userId) {
         try {
-            const { userId } = req.body;
-            console.log('req.body:', req.body); 
             const user = await userDao.getUserById(userId);
-            res.status(200).json({
-                status: 'success',
-                user,
-                message: 'ID del usuario obtenido!',
-            });
         } catch (error) {
             console.error('Error al obtener usuario por ID:', error);
-            res.status(500).json({ error, message: 'Error al obtener usuario por ID.' });
         }
     }
 }
